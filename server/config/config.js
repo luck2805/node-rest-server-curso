29f02f5202d75134b1db56be24195c82eb90a7cf
berniluciano <luciano.berni@airbits.com.ar>
2020-08-15 18:16:36 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let url ;
 if (process.env.NODE_ENV === 'dev') {
     url = 'mongodb://localhost:27017/cafe'
 } else {
-    url = 'mongodb+srv://user:GWsJ9jCUhPhUz74@cluster0.fh7rn.mongodb.net/cafe'
+    url = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = url
