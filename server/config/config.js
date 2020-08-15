43db90e8bea06ad1d9fa32d093d40311365e21f7
berniluciano <luciano.berni@airbits.com.ar>
2020-08-15 11:52:17 -0300

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,3 @@
+// la variabale PORT esta administrada por HEroku por ej en este caso, por lo tanto si no existe estamos en un entorno de desarrollo
+// process es un objeto globan que corre en toda la aplicacion
+process.env.PORT = process.env.PORT || 3000
---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('./config/config');
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {
+    res.json('Get Usuario')
+});
+
+app.post('/usuario', (req, res) => {
+
+    const body = req.body;
+
+    if (!body.nombre) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario',
+        })
+    } else {
+        res.json(body)
+    }
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+    const id = req.params.id
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('Delete Usuario')
+});
+
+
+app.listen(process.env.PORT, () => console.log('Escuchando en el puerto:', process.env.PORT));
